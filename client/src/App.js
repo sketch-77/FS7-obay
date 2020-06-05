910050bc0527418c9a562ae6be96f8c1b18c8b0f
sketch-77 <creative@studiocolured.com>
2020-06-05 16:24:40 +0200

    initialising navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,38 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-
-import Login from "./components/Login";
+// import Login from "./components/Login";
 // import SignUp from "./components/SignUp";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}}
+    this.state = {};
+  }
 
-
-    render() {
-      return (
-        <Router>
-          <div className="App">
-            <NavBar></NavBar>
-            <div className="auth-wrapper">
-              <div className="auth-inner">
-                <Switch>
-                  {/* <Route exact path="/"><Login/></Route>  */}
-                  <Route path="/login" component={Login} />
-                  <Route path="/products" component={Login} />
-                  {/* <Route path="/sign-up"><SignUp/></Route> */}
-                  {/* <Route path="/profile" component={Profile}/> */}
-                </Switch>
-              </div>
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar></NavBar>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Switch>
+                <Route exact path="/"></Route>
+                {/* <Route path="/login" component={Login} />
+                <Route path="/products" component={Login} /> */}
+                {/* <Route path="/sign-up"><SignUp/></Route> */}
+                {/* <Route path="/profile" component={Profile}/> */}
+              </Switch>
             </div>
           </div>
-        </Router>
-      );
-    }
+        </div>
+      </Router>
+    );
   }
-  
-  export default App;
+}
+
+export default App;
