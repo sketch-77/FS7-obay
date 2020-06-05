9c5653cdd0542921bd1ed13477729a0ba98b5c8c
yakomoga <olga.dayneko@gmail.com>
2020-06-05 11:46:11 +0200

    Adding navbar and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Hello oBay!</header>
-    </div>
-  );
-}
 
-export default App;
+import Login from "./components/Login";
+// import SignUp from "./components/SignUp";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}}
+
+
+    render() {
+      return (
+        <Router>
+          <div className="App">
+            <NavBar></NavBar>
+            <div className="auth-wrapper">
+              <div className="auth-inner">
+                <Switch>
+                  <Route exact path="/"><Login/></Route> 
+                  <Route path="/login" component={Login} />
+                  <Route path="/products" component={Login} />
+                  {/* <Route path="/sign-up"><SignUp/></Route> */}
+                  {/* <Route path="/profile" component={Profile}/> */}
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </Router>
+      );
+    }
+  }
+  
+  export default App;
