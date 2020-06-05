36c70066032574f6d695e82c6b0d760e068461da
sketch-77 <creative@studiocolured.com>
2020-06-05 16:06:59 +0200

    adding user model and route structure

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("send user info");
+  res.render("homepage");
 });
 
 module.exports = router;
