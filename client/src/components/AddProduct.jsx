018421cf06f1762813e1b1cdeba6ab28b76d8d6b
yakomoga <olga.dayneko@gmail.com>
2020-06-08 15:28:15 +0200

    Adding products page

---
+++ client/src/components/AddProduct.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+
+class Protected extends Component {
+
+
+
+
+    render() {
+        return (
+            <Card>
+                <Card.Text>Protected page</Card.Text>
+            </Card>
+        );
+    }
+}
+
+export default Protected;
