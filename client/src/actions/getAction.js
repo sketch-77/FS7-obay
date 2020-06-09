d9784f5111494d5f1ef55f5aa0595fcdf26054a9
Your Name <you@example.com>
2020-06-10 02:58:40 +0300

    rebase issue

--- client/src/actions/getAction.js
+++ client/src/actions/getAction.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { GET_NUMBERS_CART } from "./types";
-=======
-import { GET_NUMBERS_CART } from "./types;";
->>>>>>> 744a5c3... front end redux
 export const getNumbers = () => {
   return (dispatch) => {
     console.log("getting all cart numbers");
