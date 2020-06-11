24bf291dc061c906a28506a1d00b7ddd731f4c0d
sketch-77 <creative@studiocolured.com>
2020-06-11 17:23:54 +0200

    images database created

--- models/images.js
+++ models/images.js
@@ -1,9 +1,14 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Images = sequelize.define('Images', {
-    id: DataTypes.STRING
-  }, {});
-  Images.associate = function(models) {
+  const Images = sequelize.define(
+    "Images",
+    {
+      productId: DataTypes.STRING,
+      path: DataTypes.STRING,
+    },
+    {}
+  );
+  Images.associate = function (models) {
     // associations can be defined here
   };
   return Images;
