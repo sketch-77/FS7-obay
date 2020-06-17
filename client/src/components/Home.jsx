c9395fb29921468be62db9bc21e6886eb5e96e91
sketch-77 <creative@studiocolured.com>
2020-06-17 10:39:07 +0200

    /component /home styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import { Route, withRouter } from "react-router-dom";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
 class Home extends Component {
   render() {
-    return <div>Welcome Home!</div>;
+    return
+    <div>Welcome Home!</div>
+    <Container>
+        <Row xs={2} md={4} lg={6}>
+          <Col>
+            {" "}
+            <h1>Login</h1>
+          </Col>
+        </Row>
+
+        <Card>
+          <Card.Body></Card.Body>
+          </Card.Body>
+        </Card>
+      </Container>
   }
 }
 
-export default Home;
+export default withRouter(Home);
