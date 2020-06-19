b8585d6697cfc76b44b335eff883238318e65213
yakomoga <olga.dayneko@gmail.com>
2020-06-19 11:20:38 +0200

    fixing search to lowercase and adding icon

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,7 +6,6 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 // import Card from "react-bootstrap/Card";
 // import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import Carousel from "react-bootstrap/Carousel";
 import Card from "react-bootstrap/Card";
@@ -14,27 +13,22 @@ import Card from "react-bootstrap/Card";
 class Home extends Component {
   render() {
     return (
-      <div className="auth-wrapper">
-        <div className="mb-5"></div>
-        <Row className="justify-content-md-center">
-          <Col>
+      <div>
+        <div className="mb-5">
             <Image
-              src="/Asset6.png"
-              height="1200"
+              src="/Asset0.png"
+              fluid
               className="d-inline-block align-center"
               alt="AfriKbay-logo"
             />
-          </Col>
-        </Row>
-
-        <div className="auth-wrapper">
+        </div>
           <div className="mb-5">
             <Carousel>
               <Carousel.Item>
                 <img
                   className="d-block w-100"
                   src="/Asset3.png"
-                  Width="400"
+                  width="400"
                   text="First slide&bg=282c34"
                   alt="Made in Kenya"
                 />
@@ -59,7 +53,6 @@ class Home extends Component {
               </Carousel.Item>
             </Carousel>
           </div>
-        </div>
 
         <div className="comments">
           <Card>
